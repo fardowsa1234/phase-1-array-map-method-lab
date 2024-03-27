e931bf46a1cbff79c8c845acea3a99c9cb51c8d3
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-03-27 08:57:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+    "what does the this keyword mean?",
+    "Implementing Blockchain web API",
+    "what is the constructor OO pattern?",
+    "The Test Driven Development Workflow",
+    "what is NaN and how Can we Check for it",
+    "what is the difference between stopPropagation and preventDefault?",
+    "Immutable state and pure functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?",
 
+];
 const titleCased = () => {
-  return tutorials
+return tutorials.map((sent)=> {
+    const words=sent.split(" ");
+    console.log(words)
+    const titlecaseWord= words.map((word)=>{
+        console.log(word)
+        return word.charAt(0).toUpperCase()+ word.slice(1)
+
+    })
+    const result=titlecaseWord.join(" ")
+
+    return result
+})
+
 }
